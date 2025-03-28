50d5f08593b429bbd692b3a5f8a77aec02e00253
1967cooder <silvanalindholm@hotmail.com>
2025-03-28 13:07:57 +0200

    testi

---
+++ week 1/skripti.js
@@ -0,0 +1,17 @@
+console.log("Tämä on Javascript-testi.");
+alert("Moi");
+
+const muuttuja = "Hello world";
+// ei pysty muttamaan arvoa myöhemmin
+
+function funktio() {
+let x, y, z;
+(x = 5), (y=2)
+z = x + y
+
+console.log("This is z:", z);
+
+}
+
+//Kutsu funktio näin:
+funktio();
