6bde21d1625eb05f001e2a55131fd797e0521ab0
1967cooder <silvanalindholm@hotmail.com>
2025-04-07 11:52:48 +0300

    test

---
+++ JS/week3/silmukat/silmukat_01/skripti.js
@@ -0,0 +1,12 @@
+//Tee funktio nimeltä logOddNumbers:
+// - Se tulostaa konsoliin kaikki parittomat numerot 1 ja 100 välillä.
+// Eli 1, 3, 5, 7, 9, 11 jne. lukuun 99 asti.
+
+function logOddNumbers (laskuri){
+    let laskuri = 1
+while(laskuri >= 100) {
+    console.log(laskuri)
+    laskuri+2 
+}
+
+}
