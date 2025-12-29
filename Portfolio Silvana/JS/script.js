dd610b158c67566cd32b1c033a67fdffc0a02feb
1967cooder <silvanalindholm@hotmail.com>
2025-12-29 18:11:26 +0200

    added new form in contact.html, added new file thank-you.html, the js-code for contact form is commented

--- Portfolio Silvana/JS/script.js
+++ Portfolio Silvana/JS/script.js
@@ -1,54 +1,39 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //userForm----------------------------------------------------------------
 
-const form = document.getElementById("userForm");
-const errorMessage = document.getElementById("errorMessage");
+// const form = document.getElementById("userForm");
+// const errorMessage = document.getElementById("errorMessage");
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault()//lopettaa lataamista
-  
-  // Haetaan kenttien arvot kun lähetetään lomakkeen l
-  const name = document.getElementById("yourName").value
-  const email= document.getElementById("yourEmail").value;
-  const message = document.getElementById("messageToMe").value;
- 
-// Tyhjennä mahdollinen aiempi viesti
- errorMessage.textContent = "";
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault()//lopettaa lataamista
 
- 
-  if (email.trim() === "") {
-    event.preventDefault(); // Estää lomakkeen lähetyksen
-    errorMessage.textContent = "Email field cannot be empty!";
-    errorMessage.style.color = "red";
-  return;//Lopettaa lähettämista jos email on tyhjä
+//   // Haetaan kenttien arvot kun lähetetään lomakkeen l
+//   const name = document.getElementById("yourName").value
+//   const email= document.getElementById("yourEmail").value;
+//   const message = document.getElementById("messageToMe").value;
 
-  }
+// // Tyhjennä mahdollinen aiempi viesti
+//  errorMessage.textContent = "";
 
-//jos kaikki on ok
-    alert('Thank you, ' + name + '! Your message has been sent.');// Näyttää viesti
+//   if (email.trim() === "") {
+//     event.preventDefault(); // Estää lomakkeen lähetyksen
+//     errorMessage.textContent = "Email field cannot be empty!";
+//     errorMessage.style.color = "red";
+//   return;//Lopettaa lähettämista jos email on tyhjä
 
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Message:', message);
+//   }
 
-form.reset();
+// //jos kaikki on ok
+//     alert('Thank you, ' + name + '! Your message has been sent.');// Näyttää viesti
 
-});
+//     console.log('Name:', name);
+//     console.log('Email:', email);
+//     console.log('Message:', message);
+
+// form.reset();
+
+// });
 
 //nappi ylös--------------------------------------------------------
-document.getElementById("toTopBtn").addEventListener("click", function() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+document.getElementById("toTopBtn").addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
