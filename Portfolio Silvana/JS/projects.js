78c962ab1359af92db4dc4cc86f47d881d75e1e6
1967cooder <silvanalindholm@hotmail.com>
2025-12-29 21:37:00 +0200

    added new Form for Netlify to about.html, education.html,html_css.html,index.html,JavaScript.html,Others.html,projects.html, added changes to projects.js, added new files react_wordpress.js and React_WordPress.html

--- Portfolio Silvana/JS/projects.js
+++ Portfolio Silvana/JS/projects.js
@@ -64,36 +64,36 @@ function toggleTheme() {
 
 //userForm----------------------------------------------------------------
 
-const form = document.getElementById("userForm");
-const errorMessage = document.getElementById("errorMessage");
+// const form = document.getElementById("userForm");
+// const errorMessage = document.getElementById("errorMessage");
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault(); //lopettaa lataamista
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault(); //lopettaa lataamista
 
-  // Haetaan kenttien arvot kun lähetetään lomakkeen l
-  const name = document.getElementById("yourName").value;
-  const email = document.getElementById("yourEmail").value;
-  const message = document.getElementById("messageToMe").value;
+//   // Haetaan kenttien arvot kun lähetetään lomakkeen l
+//   const name = document.getElementById("yourName").value;
+//   const email = document.getElementById("yourEmail").value;
+//   const message = document.getElementById("messageToMe").value;
 
-  // Tyhjennä mahdollinen aiempi viesti
-  errorMessage.textContent = "";
+//   // Tyhjennä mahdollinen aiempi viesti
+//   errorMessage.textContent = "";
 
-  if (email.trim() === "") {
-    event.preventDefault(); // Estää lomakkeen lähetyksen
-    errorMessage.textContent = "Email field cannot be empty!";
-    errorMessage.style.color = "red";
-    return; //Lopettaa lähettämista jos email on tyhjä
-  }
+//   if (email.trim() === "") {
+//     event.preventDefault(); // Estää lomakkeen lähetyksen
+//     errorMessage.textContent = "Email field cannot be empty!";
+//     errorMessage.style.color = "red";
+//     return; //Lopettaa lähettämista jos email on tyhjä
+//   }
 
-  //jos kaikki on ok
-  alert("Thank you, " + name + "! Your message has been sent."); // Näyttää viesti
+//   //jos kaikki on ok
+//   alert("Thank you, " + name + "! Your message has been sent."); // Näyttää viesti
 
-  console.log("Name:", name);
-  console.log("Email:", email);
-  console.log("Message:", message);
+//   console.log("Name:", name);
+//   console.log("Email:", email);
+//   console.log("Message:", message);
 
-  form.reset();
-});
+//   form.reset();
+// });
 
 //nappi ylös--------------------------------------------------------
 document.getElementById("toTopBtn").addEventListener("click", function () {
